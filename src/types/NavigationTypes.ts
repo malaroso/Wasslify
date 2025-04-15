9795e8559179d2f95b18e508f1a952b1555ce44e
Emirhan Ekin <ekinees@hotmail.com>
2025-04-15 10:13:06 +0300

    Added new navigation types

--- src/types/NavigationTypes.ts
+++ src/types/NavigationTypes.ts
@@ -1,12 +1,20 @@
 export type RootStackParamList = {
     Welcome: undefined;
     Login: undefined;
+    Register: undefined;
     Home: undefined;
+    WallpaperDetail: { wallpaperId: number };
+    CategoryDetail: { categoryId: number };
+    Subscription: undefined;
+    Profile: undefined;
+    Settings: undefined;
     Premium: undefined;
     Favorites: undefined;
-    Profile: undefined;
     PasswordChange: undefined;
     ProfileUpdate: undefined;
     CleanScreen: undefined;
     PixabayScreen: undefined;
+    Payment: undefined;
+    Faq: undefined;
+    WallpaperHome: undefined;
 };
