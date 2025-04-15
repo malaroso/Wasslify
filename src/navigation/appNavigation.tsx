d618a8db790e227b999e282ea525fa32e9ba37fe
Emirhan Ekin <ekinees@hotmail.com>
2025-04-15 10:14:58 +0300

    Added new routes

--- src/navigation/appNavigation.tsx
+++ src/navigation/appNavigation.tsx
@@ -12,6 +12,11 @@ import PasswordChangeScreen from '../screens/auth/PasswordChangeScreen';
 import ProfileUpdate from '../screens/auth/ProfileUpdate';
 import CleanScreen from '../screens/CleanScreen';
 import PixabayScreen from '../screens/PixabayScreen';
+import SubscriptionScreen from '../screens/profile/SubscriptionScreen';
+import PaymentScreen from '../screens/profile/PaymentScreen';
+import FaqScreen from '../screens/profile/FaqScreen';
+import WallpaperHomeScreen from '../screens/WallpaperHomeScreen';
+
 const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigation = () => {
@@ -29,12 +34,16 @@ const AppNavigation = () => {
                 ) : (
                     <>
                         <Stack.Screen name="Home" component={HomeScreen} />
+                        <Stack.Screen name="WallpaperHome" component={WallpaperHomeScreen} />
                         <Stack.Screen name="Favorites" component={FavoritesScreen} />
                         <Stack.Screen name="Profile" component={ProfileScreen} />
                         <Stack.Screen name="PasswordChange" component={PasswordChangeScreen} />
                         <Stack.Screen name="ProfileUpdate" component={ProfileUpdate} />
                         <Stack.Screen name="PixabayScreen" component={PixabayScreen} />
                         <Stack.Screen name="CleanScreen" component={CleanScreen} />
+                        <Stack.Screen name="Subscription" component={SubscriptionScreen} />
+                        <Stack.Screen name="Payment" component={PaymentScreen} />
+                        <Stack.Screen name="Faq" component={FaqScreen} />
                     </>
                 )}
             </Stack.Navigator>
